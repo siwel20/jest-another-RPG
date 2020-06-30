89de5580a1dcbc8cea8bf30a1de18b08d5978235
siwel20 <plthrock@gmail.com>
2020-06-29 21:05:25 -0600

    set up testing frame work

---
+++ __tests__/random.test.js
@@ -0,0 +1,7 @@
+const randomNumber = require('../lib/random.js');
+const random = require('../lib/random.js');
+
+test('gets random number between 1 and 10', () => {
+  expect(randomNumber()).toBeGreaterThanOrEqual(1);
+  expect(randomNumber()).toBeLessThanOrEqual(10);
+});
